cb56d5ce0838373f72a1df36c73c80554a245ea3
muntaha747 <muntaha747@gmail.com>
2026-09-25 00:51:19 -0400

    rbac

--- Terraform/IAM-User.tf
+++ Terraform/IAM-User.tf
@@ -19,3 +19,23 @@ resource "aws_iam_user_policy" "DevOps-User-Inline-Policy" {
     ]
   })
 }
+
+
+##############################################################################
+# RBAC for the DevOps-User Because it was the root user who created the EKS Infra so the DevOps User cannot access the cluster so we need the authorization for the devops user to access the entire cluster.
+###############################################################################
+resource "aws_eks_access_entry" "devops_user" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_user.DevOps-User.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "devops_user_admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_eks_access_entry.devops_user.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
