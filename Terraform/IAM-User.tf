6726b8b6d882fb481dd9a23c070faa8cd0210373
muntaha747 <muntaha747@gmail.com>
2026-09-25 00:27:44 -0400

    .gitignore

--- Terraform/IAM-User.tf
+++ Terraform/IAM-User.tf
@@ -1,14 +1,9 @@
 resource "aws_iam_user" "DevOps-User" {
   name = "DevOps-User"
 }
-
-
-
 resource "aws_iam_access_key" "DevOps-User" {
   user = aws_iam_user.DevOps-User.name
 }
-
-
 resource "aws_iam_user_policy" "DevOps-User-Inline-Policy" {
   name = "test"
   user = aws_iam_user.DevOps-User.name
